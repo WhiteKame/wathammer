a28d8ddaebfc3555cf6adbbdf9fa69ad0353bc8c
WhiteKame <lujiayulu1998@gmail.com>
2023-07-05 23:26:37 +0800

    修改UI

--- index.py
+++ index.py
@@ -1,141 +1,190 @@
 from dash import dcc, html
 import dash_bootstrap_components as dbc
+from function import create_slider
 
-index = html.Div([
-    html.H1('甲鱼的数学战锤'),
+# 输入攻击次数和伤害
+attack_input = dbc.Input(
+    id='attack_input',
+    className='input_name',
+    type='text',
+    placeholder='输入 {x} {xD6} {xD6+x}'
+)
 
+damage_input = dbc.Input(
+    id='damage_value',
+    className='input_name',
+    type='text',
+    placeholder='输入 {x} {xD6} {xD6+x}'
+)
+
+# 按钮
+run_button = dbc.Button('运行', id='run-button', color='primary')
+reset_button = dbc.Button('重置', id='reset-button', color='light')
+buttons_div = html.Div([
+    run_button,
+    reset_button
+], className="d-flex justify-content-between")
+
+attack_input_div = html.Div([
+    html.P('输入攻击次数:'),
+    attack_input,
+    html.P('输入单次伤害:'),
+    damage_input,
+    buttons_div,
+], className="p-3 text-white bg-dark rounded-3 mb-3")
+
+# 输入数据表的内容
+hit_input = create_slider('hit_value', 2, 6, 1, 4)
+wound_input = create_slider('wound_value', 2, 6, 1, 4)
+
+save_input = dcc.Slider(
+    2, 7, 1,
+    value=4,
+    marks={
+        2: {'label': '2+'},
+        3: {'label': '3+'},
+        4: {'label': '4+'},
+        5: {'label': '5+'},
+        6: {'label': '6+'},
+        7: {'label': '过穿', 'style': {'color': '#f50'}}
+    },
+    id='armor_value',
+    included=True
+)
+
+pain_input = dcc.Slider(
+    2, 7, 1,
+    value=7,
+    marks={
+        2: {'label': '2+'},
+        3: {'label': '3+'},
+        4: {'label': '4+'},
+        5: {'label': '5+'},
+        6: {'label': '6+'},
+        7: {'label': "没有", 'style': {'color': '#f50'}}
+    },
+    id='pain_value',
+    included=True
+)
+
+datasheet_input_div = html.Div([
+    html.P('输入命中值:'),
+    hit_input,
+    html.P('输入造伤值:'),
+    wound_input,
+    html.P('输入保护值:'),
+    save_input,
+    html.P('输入不怕疼:'),
+    pain_input,
+])
+
+# 输入修正值
+hit_modify_input = dcc.Slider(
+    -1, 1, 1,
+    value=0,
+    marks={
+        -1: {'label': '-1'},
+        0: {'label': '0'},
+        1: {'label': '+1'}
+    },
+    id='hit_modify'
+)
+
+wound_modify_input = dcc.Slider(
+    -1, 1, 1,
+    value=0,
+    marks={
+        -1: {'label': '-1'},
+        0: {'label': '0'},
+        1: {'label': '+1'}
+    },
+    id='wound_modify'
+)
+
+save_modify_input = dcc.Slider(
+    -1, 1, 1,
+    value=0,
+    marks={
+        -1: {'label': '-1'},
+        0: {'label': '0'},
+        1: {'label': '+1'}
+    },
+    id='armor_modify'
+)
+
+modify_input_div = html.Div([
+    html.P('输入命中修正:'),
+    hit_modify_input,
+    html.P('输入造伤修正:'),
+    wound_modify_input,
+    html.P('输入保护修正:'),
+    save_modify_input,
+])
+
+# 数值输入
+data_input_div = html.Div([
+    dbc.Tabs([
+        dbc.Tab(datasheet_input_div, label="数值"),
+        dbc.Tab(modify_input_div, label="修正")
+    ])
+])
+
+# 通用特效
+sp_rule = html.Div([
+    dbc.Checkbox(id="reroll_hits",
+                 label="全重投命中",
+                 value=False, ),
+    dbc.Checkbox(id="reroll_hit1",
+                 label="重投命中1",
+                 value=False, ),
+    dbc.Checkbox(id="reroll_wounds",
+                 label="全重投造伤",
+                 value=False, )
+], className="input_name")
+
+# 特殊武器规则
+sp_weapon_rule = html.Div([
+    dbc.Checkbox(id="combo_strike",
+                 label="连击1",
+                 value=False, ),
+    dbc.Checkbox(id="mortal_wound",
+                 label="毁灭伤害",
+                 value=False, )
+], className="input_name")
+
+# 特殊规则输入
+sp_rule_input_div = html.Div([
+    dbc.Tabs([
+        dbc.Tab(sp_rule, label="通用特效"),
+        dbc.Tab(sp_weapon_rule, label="武器特效")
+    ])
+], className="p-3")
+
+# 图表
+graph_div = html.Div([
+    dcc.Graph(id='simulation-graph')
+], style={'height': '100vh', 'overflow': 'scroll'})
+
+# 输出页
+layout = html.Div([
     dbc.Row([
-        dbc.Col([
-            html.Div([
-                html.P('输入攻击次数:'),
-                dbc.Input(id='attack_input', className='input_name', type='text', placeholder='输入x, xD6, xD6+x'),
-                html.P('输入单次伤害:'),
-                dbc.Input(id='damage_value', className='input_name', type='text', placeholder='输入x, xD6, xD6+x'),
-                dbc.Button('运行', id='run-button', color='primary'),
-                dbc.Button('重置', id='reset-button', color='light')
-            ],
-                className="h-100 p-5 text-white bg-dark rounded-3 mb-3", ),
-        ],
-            className="mb-3", xs=12, md=4),
-        dbc.Col([
-            html.Div([
-                dbc.Tabs([
-                    dbc.Tab(
-                        html.Div([
-                            html.P('输入命中值:'),
-                            # dcc.Input(id='hit_value', type='number', value=4),
-                            dcc.Slider(2, 6, 1,
-                                       value=4,
-                                       id='hit_value'
-                                       ),
-                            html.P('输入造伤值:'),
-                            # dcc.Input(id='wound_value', type='number', value=4),
-                            dcc.Slider(2, 6, 1,
-                                       value=4,
-                                       id='wound_value'
-                                       ),
-                            html.P('输入保护值:'),
-                            # dcc.Input(id='armor_value', type='number', value=4),
-                            dcc.Slider(2, 7, 1,
-                                       value=4,
-                                       marks={
-                                           2: {'label': '2+'},
-                                           3: {'label': '3+'},
-                                           4: {'label': '4+'},
-                                           5: {'label': '5+'},
-                                           6: {'label': '6+'},
-                                           7: {'label': '过穿', 'style': {'color': '#f50'}}
-                                       },
-                                       id='armor_value',
-                                       included=True
-                                       ),
-                            html.P('输入不怕疼:'),
-                            # dcc.Input(id='pain_value', type='number', value=7),
-                            dcc.Slider(2, 7, 1,
-                                       value=7,
-                                       marks={
-                                           2: {'label': '2+'},
-                                           3: {'label': '3+'},
-                                           4: {'label': '4+'},
-                                           5: {'label': '5+'},
-                                           6: {'label': '6+'},
-                                           7: {'label': "没有", 'style': {'color': '#f50'}}
-                                       },
-                                       id='pain_value',
-                                       included=True
-                                       ),
-                        ]), label='数值'
-                    ),
-                    dbc.Tab(
-                        html.Div([
-                            html.P('输入命中修正:'),
-                            dcc.Slider(-1, 1, 1,
-                                       value=0,
-                                       marks={
-                                           -1: {'label': '-1'},
-                                           0: {'label': '0'},
-                                           1: {'label': '+1'}
-                                       },
-                                       id='hit_modify'
-                                       ),
-                            html.P('输入造伤修正:'),
-                            dcc.Slider(-1, 1, 1,
-                                       value=0,
-                                       marks={
-                                           -1: {'label': '-1'},
-                                           0: {'label': '0'},
-                                           1: {'label': '+1'}
-                                       },
-                                       id='wound_modify'
-                                       ),
-                            html.P('输入保护修正:'),
-                            dcc.Slider(-1, 1, 1,
-                                       value=0,
-                                       marks={
-                                           -1: {'label': '-1'},
-                                           0: {'label': '0'},
-                                           1: {'label': '+1'}
-                                       },
-                                       id='armor_modify'
-                                       ),
-                        ]), label='修正'
-                    ),
+        dbc.Col([  # 左侧列
+            html.H1('甲鱼的数学战锤 BETA v0.1'),
+            dbc.Row([
+                dbc.Col([
+                    attack_input_div
                 ]),
-            ], className="h-100 p-5 bg-light border rounded-3 mb-3"),
+                dbc.Col([
+                    sp_rule_input_div
+                ])
+            ], className='row-eq-height'),
+            data_input_div
         ],
-            className="mb-3", xs=12, md=4
-        ),
-        dbc.Col([
-            html.Div([
-                dbc.Tabs([
-                    dbc.Tab(html.Div([
-                        dbc.Checkbox(id="reroll_hits",
-                                     label="全重投命中",
-                                     value=False, ),
-                        dbc.Checkbox(id="reroll_hit1",
-                                     label="重投命中1",
-                                     value=False, ),
-                        dbc.Checkbox(id="reroll_wounds",
-                                     label="全重投造伤",
-                                     value=False, )
-                    ], className="input_name"), label='通用特效'
-                    ),
+            xs=12, md=6),  # 调整列宽比例
 
-                    dbc.Tab(
-                        html.Div([
-                            dbc.Checkbox(id="combo_strike",
-                                         label="连击1",
-                                         value=False, )
-                        ], className="input_name"),
-                        label='武器特效'),
-                ]),
-            ],
-                className="h-100 p-5 bg-light border rounded-3 mb-3",
-            ),
+        dbc.Col([  # 右侧列
+            graph_div
         ],
-            className="mb-3", xs=12, md=4),
-    ]),
-    html.Div([
-        dcc.Graph(id='simulation-graph')
-    ]),
+            xs=12, md=6),  # 调整列宽比例
+    ])
 ])
